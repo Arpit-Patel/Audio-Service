b0b4e9707325286ccece1f68afb6724b00625ef2
Arpit-Patel <a285pate@uwaterloo.ca>
2019-01-15 20:15:24 -0500

    Security - XSS prevention added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sanitizer = require('sanitize');
 
 var mainRouter = require('./routes/routes');
 
@@ -11,6 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sanitizer.middleware);
 
 app.use('/', mainRouter);
 
